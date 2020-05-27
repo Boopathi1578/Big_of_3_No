8d44e83d4778bcbf20306baf9bc8c0200980c5cc
Boopathi1578 <60506343+Boopathi1578@users.noreply.github.com>
2020-05-27 12:55:42 +0530

    Big_of_3

---
+++ Bigof3.py
@@ -0,0 +1,10 @@
+number1=int(input("enter the value"))
+number2=int(input("enter the value"))
+number3=int(input("enter the vale"))
+if((number1<number2)and(number1>number3)):
+  print("number1 is big",number1); 
+elif((number2>number3)and(number2>number1)):
+  print("number2 is big",number2);
+else:
+  print("number3 is big",number3);
+
